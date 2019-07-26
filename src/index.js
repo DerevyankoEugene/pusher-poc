a774920e27c44e0d6c10b076a8acbc35b69fc64b
Eugene Derevyanko <derevyanko.eugene@gmail.com>
2019-07-26 14:01:01 +0300

    implemented poc

---
+++ src/index.js
@@ -0,0 +1,85 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const serve = require("koa-static");
+const koaBody = require("koa-bodyparser");
+
+const Chatkit = require("@pusher/chatkit-server");
+
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:77f76389-a828-4605-bb7d-083f9b1383a3",
+  key: "abc7bc02-2bbd-429b-81f9-b8bb6485c8f4:BrVz9ThuLb5OipB+zeceHPNYjjb2/rHmNKlaIFnWsJI=",
+});
+
+const appPort = 8765;
+
+const app = new Koa();
+const router = new Router();
+
+const serveWeb = serve("./web", {});
+router.get(`/client/(.*)`, async (ctx, next) => {
+  await serveWeb(ctx, next);
+});
+
+router.post("/pusher/auth", (ctx) => {
+  const authData = chatkit.authenticate({
+    userId: ctx.request.query.user_id,
+  });
+  ctx.status = 200;
+  ctx.body = authData.body;
+});
+
+router.post("/pusher/user/create", async (ctx) => {
+  console.log(ctx.request.query);
+  await chatkit
+    .createUser({
+      id: decodeURIComponent(ctx.request.query.user_id),
+      name: decodeURIComponent(ctx.request.query.user_name),
+    })
+    .then(() => {
+      ctx.body = "User created successfully";
+      ctx.status = 200;
+    })
+    .catch((err) => {
+      ctx.body = err;
+      ctx.status = 500;
+    });
+});
+
+router.get("/pusher/user/list", async (ctx) => {
+  await chatkit
+    .getUsers()
+    .then((res) => {
+      ctx.body = res;
+      ctx.status = 200;
+    })
+    .catch((err) => {
+      ctx.body = err;
+      ctx.status = 500;
+    });
+});
+
+router.post("/pusher/room/create", async (ctx) => {
+  console.log(ctx.request.query);
+  const hash = new Date().getTime();
+  const roomId = decodeURIComponent(`custom id ${hash}`);
+  await chatkit.createRoom({
+    id: roomId,
+    creatorId: decodeURIComponent(ctx.request.query.creator_id),
+    name: `custom name ${ctx.request.query.creator_id} ${hash}`,
+    customData: { foo: 42 },
+  });
+  await chatkit.addUsersToRoom({
+    roomId: roomId,
+    userIds: ctx.request.query.user_ids.split(",").map((id) => decodeURIComponent(id)),
+  });
+  const room = await chatkit.getRoom({ roomId });
+  ctx.body = room;
+  ctx.status = 200;
+});
+
+app.use(koaBody());
+app.use(router.allowedMethods());
+app.use(router.routes());
+app.listen(appPort);
+
+global.console.log(`started on ${appPort}`);
---
+++ web/client/js/index.js
@@ -0,0 +1,105 @@
+let app = {
+  currentUser: null,
+};
+
+async function initPusher(userId) {
+  const { TokenProvider, ChatManager } = Chatkit;
+  const chatManager = new ChatManager({
+    instanceLocator: "v1:us1:77f76389-a828-4605-bb7d-083f9b1383a3",
+    userId,
+    tokenProvider: new TokenProvider({ url: "/pusher/auth" }),
+  });
+  await chatManager
+    .connect()
+    .then((currentUser) => {
+      app.currentUser = currentUser;
+      const roomsText = currentUser.rooms
+        .map((r) => JSON.stringify({ id: r.id, unread: r.unreadCount }))
+        .join("<br/>");
+      document.getElementById("current-user-info").innerHTML = `CurrentUser: ${app.currentUser.name} ${
+        app.currentUser.id
+      }, <br/>rooms:<br/>${roomsText}`;
+      console.log("Successful connection", currentUser);
+    })
+    .catch((err) => {
+      console.log("Error on connection", err);
+    });
+}
+
+async function initMessaging() {
+  function addToHistory(message) {
+    const historyElm = document.getElementById("history");
+    const own = message.senderId === app.currentUser.id;
+    historyElm.innerHTML =
+      historyElm.innerHTML +
+      `<p class="message ${own ? "outgoing" : "incoming"}">${own ? "" : message.senderId + ": "}${
+        message.parts[0].payload.content
+      }</p>`;
+  }
+
+  document.getElementById("connect-room-button").addEventListener("click", async () => {
+    // on send
+    document.getElementById("send-message-button").addEventListener("click", async () => {
+      const roomId = document.getElementById("send-to-room-id").value;
+      const message = document.getElementById("message-to-send").value;
+      await app.currentUser.sendSimpleMessage({
+        text: message,
+        roomId,
+      });
+    });
+    // on receive
+    await app.currentUser.subscribeToRoomMultipart({
+      roomId: document.getElementById("send-to-room-id").value,
+      hooks: {
+        onMessage: (message) => {
+          addToHistory(message);
+        },
+      },
+    });
+  });
+}
+
+function initCreateUser() {
+  document.getElementById("create-user-button").addEventListener("click", async () => {
+    const userId = encodeURIComponent(document.getElementById("new-user-id").value);
+    const userName = encodeURIComponent(document.getElementById("new-user-name").value);
+    const url = `/pusher/user/create?user_id=${userId}&user_name=${userName}`;
+    result = await fetch(url, { method: "POST" });
+    console.log(result);
+  });
+}
+
+function initCreateRoom() {
+  document.getElementById("create-room-button").addEventListener("click", async () => {
+    const creatorId = encodeURIComponent(document.getElementById("new-room-creator_id").value);
+    const userIds = document
+      .getElementById("new-room-users")
+      .value.split(",")
+      .map((id) => encodeURIComponent(id.trim()));
+    const url = `/pusher/room/create?creator_id=${creatorId}&user_ids=${userIds}`;
+    result = await fetch(url, { method: "POST" });
+    alert(result);
+  });
+}
+
+function initConnectUser() {
+  document.getElementById("connect-button").addEventListener("click", async () => {
+    const userId = document.getElementById("connect-user-id").value;
+    await initPusher(userId);
+    await initMessaging();
+  });
+}
+
+async function loadUsers() {
+  const result = await fetch("/pusher/user/list");
+  const users = await result.json();
+  const text = users.map((u) => `<p>${u.name} (${u.id})</p>`).join("\n");
+  document.getElementById("user-list").innerHTML = text;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initCreateUser();
+  initConnectUser();
+  initCreateRoom();
+  loadUsers();
+});
